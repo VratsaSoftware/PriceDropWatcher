bf5ed0715567296ffb26381704d2afab379bc3c5
stani1940 <stanislav1940@abv.bg>
2021-01-21 13:05:19 +0200

    Create basic product`s scraper

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Goutte\Client;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/products/{id}/price', function () {
+    $client = new Client();
+    $crawler = $client->request('GET', 'https://www.emag.bg/prahosmukachka-stick-2-v-1-philips-powerpro-duo-18-v-0-6-l-tehnologija-powercyclone-siva-fc6168-01/pd/DWJPLBBBM/?ref=prod_CMP-26852_4145_44681');
+    $crawler->filter('.page-title')->each(function ($node) {
+
+        dump($node->text());
+
+    });
+   // $crawler->filterXPath('//div[@class=\'product-highlight product-page-pricing\']//p[@class=\'product-new-price\']/text()')->each(function ($node) {
+
+        //dump($node->text());
+
+    //});
+    echo "title:{$crawler->filter('.page-title')->first()->text()}";
+
+    $price_bgn = $crawler->filterXPath('//div[@class=\'product-highlight product-page-pricing\']//p[@class=\'product-new-price\']/text()')->first()->text();
+
+    $price_stotinki = $crawler->filterXPath('//div[@class=\'product-highlight product-page-pricing\']//p[@class=\'product-new-price\']/sup/text()')->first()->text();
+    //dd($price_stotinki);
+    $price = (float)$price_bgn . '.' .$price_stotinki;
+    echo "<p>price:{$price}</p>";
+    dd($price);
+
 });
