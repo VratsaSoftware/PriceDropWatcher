42fea0104424d60a23bdddb192bf72820b323c36
kimarinov <58824713+kimarinov@users.noreply.github.com>
2021-01-27 19:09:52 +0200

    sunk
    x

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,37 +4,47 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Web Scraper</title>
+
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
     <style>
         .glyphicon.fast-right-spinner {
             -webkit-animation: glyphicon-spin-r 1s infinite linear;
             animation: glyphicon-spin-r 1s infinite linear;
         }
+
         @-webkit-keyframes glyphicon-spin-r {
             0% {
                 -webkit-transform: rotate(0deg);
                 transform: rotate(0deg);
             }
+
             100% {
                 -webkit-transform: rotate(359deg);
                 transform: rotate(359deg);
             }
         }
+
         @keyframes glyphicon-spin-r {
             0% {
                 -webkit-transform: rotate(0deg);
                 transform: rotate(0deg);
             }
+
             100% {
                 -webkit-transform: rotate(359deg);
                 transform: rotate(359deg);
             }
         }
     </style>
+
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
@@ -51,6 +61,7 @@
                         </button>
                         <a class="navbar-brand" href="{{url('/')}}">Web Scraper</a>
                     </div>
+
                     <div class="collapse navbar-collapse">
                         <ul class="nav navbar-nav navbar-right">
                             <li class="dropdown">
@@ -68,10 +79,13 @@
                     </div>
                 </div>
             </nav>
+
+
                 @yield('content')
             </div>
         </div>
     </div>
+
     @yield('script')
 </body>
 </html>
