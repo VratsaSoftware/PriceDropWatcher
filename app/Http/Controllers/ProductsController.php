01c5b0509641aa33cbf2cbdf965bb6f73ba7f59a
stani1940 <stanislav1940@abv.bg>
2021-02-04 23:58:36 +0200

    Get status code of response

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use http\Client\Response;
 use Illuminate\Http\Request;
 use Goutte\Client;
+
 class ProductsController extends Controller
 {
     /**
@@ -30,7 +32,7 @@ class ProductsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -41,13 +43,13 @@ class ProductsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Product  $product
+     * @param \App\Product $product
      * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
     {
         $get_data = $this->scrape_data()->content();
-        $data = json_decode($get_data,true);
+        $data = json_decode($get_data, true);
         //dd($data);
         return view('products.single_product', compact('data'));
     }
@@ -55,7 +57,7 @@ class ProductsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Product  $product
+     * @param \App\Product $product
      * @return \Illuminate\Http\Response
      */
     public function edit(Product $product)
@@ -66,8 +68,8 @@ class ProductsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Product $product
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Product $product)
@@ -78,34 +80,43 @@ class ProductsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Product  $product
+     * @param \App\Product $product
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $product)
     {
         //
     }
+
     public function scrape_data()
     {
         $details = array();
         $client = new Client();
-        $url = 'https://www.emag.bg/prahosmukachka-stick-2-v-1-philips-powerpro-duo-18-v-0-6-l-tehnologija-powercyclone-siva-fc6168-01/pd/DWJPLBBBM/?ref=prod_CMP-26852_4145_44681';
-        $crawler = $client->request('GET', $url);
+        try {
+            $url = 'https://www.emag.bg/prahosmukachka-stick-2-v-1-philips-powerpro-duo-18-v-0-6-l-tehnologija-powercyclone-siva-fc6168-01/pd/DWJPLBBBM/?ref=prod_CMP-26852_4145_44681';
+            $crawler = $client->request('GET', $url);
 
-        $title = $crawler->filter('.page-title')->first()->text();
-        $price_bgn = $crawler->filterXPath('//div[@class=\'product-highlight product-page-pricing\']//p[@class=\'product-new-price\']/text()')->first()->text();
-        $price_stotinki = $crawler->filterXPath('//div[@class=\'product-highlight product-page-pricing\']//p[@class=\'product-new-price\']/sup/text()')->first()->text();
-        //dd($price_stotinki);
-        $price = (float)$price_bgn . '.' . $price_stotinki;
+            $title = $crawler->filter('.page-title')->first()->text();
+            $price_bgn = $crawler->filterXPath('//div[@class=\'product-highlight product-page-pricing\']//p[@class=\'product-new-price\']/text()')->first()->text();
+            $price_stotinki = $crawler->filterXPath('//div[@class=\'product-highlight product-page-pricing\']//p[@class=\'product-new-price\']/sup/text()')->first()->text();
+            //dd($price_stotinki);
+            $price = (float)$price_bgn . '.' . $price_stotinki;
 
-        //dd($price);
-        $img = $crawler->filterXPath('//a[@class=\'thumbnail product-gallery-image gtm_rp125918\']//img/@src'
-        )->first()->text();
+            //dd($price);
+            $img = $crawler->filterXPath('//a[@class=\'thumbnail product-gallery-image gtm_rp125918\']//img/@src'
+            )->first()->text();
+            array_push($details, $title, $img, $price);
+            $result = $this->return_result($details);
 
-        array_push($details, $title, $img, $price);
-        $result = $this->return_result($details);
-        return response($result, 200);
-
+            return response($result, 200);
+        } catch (\InvalidArgumentException $e) {
+            $res = $client->getResponse();
+            dd($res->getStatusCode());
+            if ($res->getStatusCode() !== 200) {
+                dd($res->getStatusCode());
+                return $res->getStatusCode();
+            }
+        }
     }
 
     private function return_result($details)
