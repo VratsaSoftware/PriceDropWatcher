71cde960ab420910aaafee10a033b74ea8b313a6
stani1940 <stanislav1940@abv.bg>
2021-08-27 14:30:44 +0300

    Fix scrape method

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,7 @@ use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\BrowserKit\Client;
+use Symfony\Component\BrowserKit\HttpBrowser;
 
 class UsersController extends Controller
 {
@@ -126,19 +127,50 @@ class UsersController extends Controller
         if (!$request->link_id)
             return;
 
+
         $link = Product::find($request->link_id);
 
-        $scraper = new Scraper(new Client());
+        try {
 
-        $scraper->handle($link);
 
-        if ($scraper->status == 1) {
-            return response()->json(['status' => 1, 'msg' => 'Scraping done']);
-        } else {
-            return response()->json(['status' => 2, 'msg' => $scraper->status]);
+            $details = array();
+            $client = new Client();
+
+            $url = $link->link;
+            $crawler = $client->request('GET', $url);
+            $category = $crawler->filterXPath('//ol[@class=\'breadcrumb\']//li//a')->last()->text();
+            //dd($category);
+            $title = $crawler->filter('.page-title')->first()->text();
+            $price_bgn = $crawler->filterXPath('//div[@class=\'product-highlight product-page-pricing\']//p[@class=\'product-new-price\']/text()')->first()->text();
+            $price_stotinki = $crawler->filterXPath('//div[@class=\'product-highlight product-page-pricing\']//p[@class=\'product-new-price\']/sup/text()')->first()->text();
+            //dd($price_stotinki);
+            $price = (float)$price_bgn . '.' . $price_stotinki;
+
+            //dd($price);
+            $img = $crawler->filterXPath('//a[@class=\'thumbnail product-gallery-image gtm_rp125918\']//img/@src'
+            )->first()->text();
+
+            array_push($details, $title, $img, $price);
+            $result = $this->return_result($details);
+            return response($result, 200);
+        } catch (\ErrorException $e) {
+            $res = $client->getResponse();
+            dd($res->getStatusCode());
+            if ($res->getStatusCode() !== 200) {
+                dd($res->getStatusCode());
+                return $res->getStatusCode();
+            }
         }
+    }
 
-
+    private function return_result($details)
+    {
+        $output = [];
+        $output['title'] = $details[0];
+        $output['img'] = $details[1];
+        $output['price'] = $details[2];
+        //dd($output);
+        return $output;
     }
 
 }
